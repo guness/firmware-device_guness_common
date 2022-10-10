44a6c687b761cb23397a0eec986ce12bf366ff3e
Sinan Gunes <gunes.sinan@gmail.com>
2022-10-10 22:58:26 +0300

    Enabled private products

--- additions.mk
+++ additions.mk
@@ -1,12 +1,2 @@
-# Generic overlays
-PRODUCT_PACKAGE_OVERLAYS += device/generic/car/common/overlay device/snappautomotive/common/overlay
-
-# Add non-public overlays if they exist
-$(call inherit-product-if-exists, vendor/snappautomotive/non_public/additions.mk)
-
-# Snapp Maps
-PRODUCT_PACKAGES += osmdroid
-
-# Boot Animation
-PRODUCT_COPY_FILES += \
-    device/snappautomotive/common/bootanimations/bootanimation.zip:system/media/bootanimation.zip
+# Add non-public products if they exist
+$(call inherit-product-if-exists, packages/apps/AidenEnabler/aiden-android-enabler.mk)
