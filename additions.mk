be0fb073440b154ad551344e04440f0f33711fbc
Sinan Gunes <gunes.sinan@gmail.com>
2022-10-21 13:14:27 +0300

    Enabling ADB over wifi by default

--- additions.mk
+++ additions.mk
@@ -1,2 +1,5 @@
 # Add non-public products if they exist
 $(call inherit-product-if-exists, packages/apps/AidenEnabler/aiden-android-enabler.mk)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.adb.tcp.port=5555
