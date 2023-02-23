1eb7b8fa59ad8072201e5d4cbcaa96878928be84
Sinan Gunes <gunes.sinan@gmail.com>
2023-02-23 02:35:32 +0100

    Removed USB Auth

--- additions.mk
+++ additions.mk
@@ -2,4 +2,9 @@
 $(call inherit-product-if-exists, packages/apps/AidenEnabler/aiden-android-enabler.mk)
 
 PRODUCT_PRODUCT_PROPERTIES += \
-    persist.adb.tcp.port=5555
+    persist.adb.tcp.port=5555 \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
+    persist.sys.usb.config=mtp,adb \
+    ro.secure=0 \
+    ro.adb.secure=0
